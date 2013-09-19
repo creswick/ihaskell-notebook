f6cb4f805681fb70a9de9fdf4fef021b7444431c
Adam C. Foltzer <acfoltzer@galois.com>
2013-09-19 13:52:44 -0700

    add ghcjmagic extension to launch script
    This change eliminates the need to run %load_ext ghcjmagic in a cell
    before evaluating things as Haskell code

--- ihaskellnb.sh
+++ ihaskellnb.sh
@@ -8,8 +8,8 @@
 # Edit these paths to suit your needs
 #
 
-IHNB_ROOT=~/ihaskell-notebook
-GHCJ_BINARY=$IHNB_ROOT/ghcj/dist/build/ghcj/ghcj
+IHNB_ROOT=~/src/ihaskell-notebook
+GHCJ_BINARY=$IHNB_ROOT/ghcj/dist/dist-sandbox-6f4fed69/build/ghcj/ghcj
 IPNB_MAGICS=$IHNB_ROOT/python
 
 #
@@ -19,4 +19,4 @@ IPNB_MAGICS=$IHNB_ROOT/python
 export IHNB_ROOT
 export GHCJ_BINARY
 export PYTHONPATH=$PYTHONPATH:$IPNB_MAGICS
-ipython notebook
+ipython notebook --ext=ghcjmagic
